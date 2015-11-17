e0516686e270d94ca079afef279f271750920c0a
dtrinh888 <dtrinh888@gmail.com>
2015-11-16 20:44:59 -0800

    added files

--- js/app.js
+++ js/app.js
@@ -10,4 +10,18 @@ $(function(){
 		e.preventDefault();
 		goToByScroll($(this).attr('id'));
 	});
+
+	var portfolio = function(p, pA){
+		var $portfolio = this;
+		$portfolio.p = project;
+		$portfolio.pA = projectArea;
+		$portfolio.currentProject = 0;
+
+		$portfolio.displayProject = function(){
+			if($portfolio.currentProject < 0 || $portfolio.currentProject >= $portfolio.projects.length) {
+				return false;
+			}
+		}; 
+	};
+
 });
---
+++ js/projects.js
@@ -0,0 +1,7 @@
+var projects = [];
+
+projects.push({
+	title: 'New York Times Hack',
+	img: src='img/newyorktimeshack.jpg',
+	desc: "test description"
+});
